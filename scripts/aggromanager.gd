e1ac1884abe8cf056ad841ef716db82d780a654a
Kei <54135223+keidukebergman@users.noreply.github.com>
2026-05-22 09:22:37 +0200

    Add notice distance

--- scripts/aggromanager.gd
+++ scripts/aggromanager.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name AggroManager
 
+var notice_distance:float = 1
+var player:Node3D = null
 var target:Node3D = null
 var target_request_cooldown = 0.3
 
@@ -8,9 +10,11 @@ signal requested_player_target(requester:AggroManager)
 
 func _process(delta: float) -> void:
 	target_request_cooldown = move_toward(target_request_cooldown, 0, delta)
-	if target == null && target_request_cooldown <= 0:
+	if player == null && target_request_cooldown <= 0:
 		requested_player_target.emit(self as AggroManager)
 		target_request_cooldown = randf_range(0.3, 5)
+	if player != null && global_position.distance_to(player.global_position) < notice_distance:
+		target = player
 
 func receive_player_target(player_node:Node3D):
-	target = player_node
+	player = player_node
