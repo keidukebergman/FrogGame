58ccaf44ee7f0356945cbde01d4e70786795a529
Kei <54135223+keidukebergman@users.noreply.github.com>
2026-05-24 13:38:03 +0200

    Add slomo

---
+++ scripts/slow_mo_manager.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_C):
+		_slow_time(0.2, 0.03)
+
+func _slow_time(to_amount:float, recovery_time:float):
+	Engine.time_scale = 1 * to_amount;
+	await get_tree().create_timer(recovery_time * Engine.time_scale).timeout
+	while Engine.time_scale < 1:
+		Engine.time_scale = move_toward(Engine.time_scale, 1, 0.01 + Engine.time_scale/10)
+		await get_tree().create_timer(recovery_time * Engine.time_scale).timeout
