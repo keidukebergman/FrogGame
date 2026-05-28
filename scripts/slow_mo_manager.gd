00030b26f7c56dbeb85baa38aaa74aa1ae77e78b
Kei Duke Bergman <keidukebergman@gmail.com>
2026-05-28 22:54:18 +0200

    Add zoom manager, remove unnecessary functions from slow mo manager

--- scripts/slow_mo_manager.gd
+++ scripts/slow_mo_manager.gd
@@ -1,14 +1,6 @@
 extends Node3D
 class_name SlowMoManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_C):
-		slow_time(0.2, 0.03)
-
 func slow_time(to_amount:float, recovery_time:float):
 	Engine.time_scale = 1 * to_amount;
 	await get_tree().create_timer(recovery_time * Engine.time_scale).timeout
