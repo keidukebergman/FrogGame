a4a130aa53bcfe529f0b372dd3e81ea07edfd019
Kei <54135223+keidukebergman@users.noreply.github.com>
2026-05-30 18:47:02 +0200

    Implement zoom in system

--- scenes/player_fx_relay.gd
+++ scenes/player_fx_relay.gd
@@ -4,10 +4,12 @@ class_name PlayerFXRelay
 @export var critical_damage_threshold:float = 30
 @export var significant_damage_threshold:float = 5
 @export var slow_mo_manager:SlowMoManager
+@export var zoom_manager:ZoomInManager
 
 func on_player_took_damage(damage:float) -> void:
 	if(damage > critical_damage_threshold):
 		return
 	if(damage > significant_damage_threshold):
 		slow_mo_manager.slow_time(0.1, 0.01)
+		zoom_manager.start_zoom_effect(randf_range(0.9, 1.1), 900, 10)
 		return
--- scenes/zoom_manager.gd
+++ scenes/zoom_manager.gd
@@ -1,2 +1,34 @@
 extends Node3D
 class_name ZoomInManager
+
+var standard_zoom:float
+var zoom_state : ZoomState
+var target_zoom:float
+var zoom_in_speed:float
+var zoom_out_speed:float
+@export var camera:Camera3D
+
+enum ZoomState {
+	STANDBY,
+	ZOOMING_IN,
+	ZOOMING_OUT
+}
+
+func _ready() -> void:
+	standard_zoom = camera.size
+
+func _process(delta: float) -> void:
+	if zoom_state == ZoomState.ZOOMING_IN:
+		camera.size = move_toward(camera.size, target_zoom, delta*zoom_in_speed)
+		if abs(camera.size-target_zoom) < 0.01:
+			zoom_state = ZoomState.ZOOMING_OUT
+	elif zoom_state == ZoomState.ZOOMING_OUT:
+		camera.size = move_toward(camera.size, standard_zoom, delta*zoom_out_speed)
+		if abs(camera.size-standard_zoom) < 0.01:
+			zoom_state = ZoomState.STANDBY
+
+func start_zoom_effect(zoom_factor:float, zoom_in_speed_t:float, zoom_out_speed_t:float):
+	zoom_state = ZoomState.ZOOMING_IN
+	self.zoom_in_speed = zoom_in_speed_t
+	self.zoom_out_speed = zoom_out_speed_t
+	target_zoom = zoom_factor * standard_zoom
